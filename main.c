464fcc7d036ce1a9d0c8cea3faca8127f985897a
devTKrenowiczOverholtzer <devtkrenowiczoverholtzer@gmail.com>
2024-12-07 18:27:02 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,24 @@
 // Day 1
 #include <stdio.h>
 
-int main (){
-int LocID;
-LocID=0;
-printf("Day 1/n");
 
-//List1 Locations
-//List2 Locations
+// Function Declarations
+void count_input_lines();
+void print_challenge();
+void print_Advent_Day();
+
+
+
+int main (){
+print_Advent_Day();
+print_challenge();
+count_input_lines();
+
 
             
 return 0;
 }
+
+print_Advent_Day(){};
+print_challenge(){};
+count_input_lines(){};
